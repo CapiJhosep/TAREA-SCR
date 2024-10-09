3f4542277c714fabcec1e44a8d3888b4dad5c2f6
CapiJhosep <jhoseplamec1417@gmail.com>
2024-10-09 11:13:51 -0500

    commit

--- secuenciales/09.py
+++ secuenciales/09.py
@@ -1,3 +1,12 @@
 import os
 os.system("cls")
 
+numero = int(input("Ingrese el número de 4 cifras: "))
+
+cifra1 = numero // 1000
+cifra2 = (numero // 100) % 10
+cifra3 = (numero // 10) % 10
+cifra4 = numero % 10
+suma = cifra1 + cifra2 + cifra3 + cifra4
+
+print( f"El número es {numero} y la suma de cifras es {suma}")

c630dd603e5a0eaa0fa34a083ed4fc9e393ce4da
CapiJhosep <jhoseplamec1417@gmail.com>
2024-10-11 18:33:17 -0500

    commit

--- condicionales/14.py
+++ condicionales/14.py
@@ -8,4 +8,6 @@ if numero % 2 == 0 and numero >= 100:
     descuento = 15
 else: descuento = 5
 
-print( f"El número de la tarjeta es {numero}, el monto de la compra es {monto: .2f} soles y el descuento es del {descuento}%")
+print( f"El número de la tarjeta es {numero}" )
+print( f"El monto de la compra es {monto: .2f} soles" )
+print( f"El descuento es del {descuento}%" )
--- condicionales/18.py
+++ condicionales/18.py
@@ -12,4 +12,6 @@ elif monto < 10000:
     comedor = 10000 * 60/100
     bolsa = 10000 - (salud + comedor)
 
-print( f"Se destina al Centro de Salud {salud} soles\nSe destina al Comedor de Niños {comedor} soles\nSe invierte en la Bolsa {bolsa} soles")
+print( f"Se destina al Centro de Salud {salud} soles" )
+print( f"Se destina al Comedor de Niños {comedor} soles" )
+print( f"Se invierte en la Bolsa {bolsa} soles" )

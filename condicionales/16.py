bedc094c2dd491a5750293aea5c37e06173083e8
CapiJhosep <jhoseplamec1417@gmail.com>
2024-10-13 23:14:25 -0500

    commit

--- condicionales/16.py
+++ condicionales/16.py
@@ -1,8 +1,8 @@
 import os
 os.system("cls")
 
-ingreso = float( input("Ponga su ingreso mensual: "))
-casa = float( input("Ingrese el monto de la casa: "))
+ingreso = float( input("Ponga su ingreso mensual: ") )
+casa = float( input("Ingrese el monto de la casa: ") )
 
 if ingreso < 1250.00:
     inicial = casa * 15/100
